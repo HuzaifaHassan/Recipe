40a627d15bb966dfae47dad9efd67cbf8e5d4aab
Huzaifa Hassan <huzaifahassan97@outlook.com>
2025-05-30 23:56:44 +0100

    setting up DbHandlers and Repository pattern

---
+++ Handler/DbHandler/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbHandler.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DbHandler.Data
+{
+    public class ApplicationDbContext
+    {
+    }
+}
---
+++ Recipe/Controllers/RecipeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DbHandler.Model;
+
+namespace Recipe.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetRecipes()
+        {
+            // This is a placeholder for the actual implementation.
+            // In a real application, you would retrieve recipes from a database or other data source.
+            return Ok(new[] { "Recipe1", "Recipe2", "Recipe3" });
+        }
+        [HttpPost]
+        public IActionResult CreateRecipe(RecipeModel _recipemodel)
+        {
+            // This is a placeholder for the actual implementation.
+            // In a real application, you would save the recipe to a database or other data source.
+            //return CreatedAtAction(nameof(GetRecipes), new { id = 1 }, recipe);
+            return Ok();
+        }
+      
+    }
+}
