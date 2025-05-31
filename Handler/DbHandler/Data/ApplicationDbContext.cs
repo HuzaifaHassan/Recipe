4a545ea6799523c25122770365bbff8b0c82d4fe
Huzaifa Hassan <huzaifahassan97@outlook.com>
2025-05-31 13:55:46 +0100

    setting up 3-Tier Architecture

--- Handler/DbHandler/Data/ApplicationDbContext.cs
+++ Handler/DbHandler/Data/ApplicationDbContext.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 using DbHandler.Model;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DbHandler.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<RecipeModel> Recipes { get; set; }
+        
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    // Additional model configuration can go here
+        //}
     }
+    
 }
---
+++ Handler/DbHandler/Repositories/RecipeDetailRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using DbHandler.Model;
+using DbHandler.Data;
+
+namespace DbHandler.Repositories
+{
+    public class RecipeDetailRepository:IRecipeDetailRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public RecipeDetailRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // Implement methods for recipe details here, e.g., GetRecipeById, AddRecipe, UpdateRecipe, DeleteRecipe, etc.
+        public RecipeModel GetRecipeById(int id)
+        {
+            return _context.Recipes.Find(id);
+        }
+        public IEnumerable<RecipeModel> GetAllRecipes()
+        {
+            return _context.Recipes.ToList();
+        }
+        public void AddRecipe(RecipeModel recipe)
+        {
+            _context.Recipes.Add(recipe);
+            _context.SaveChanges();
+        }
+    }
+    
+}
